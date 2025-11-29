278d4e4214fcce6a955e36e4ffe77930861551b3
shivam kainthola <shivam.9548kainthola@gmail.com>
2025-11-30 02:52:02 +0530

    CloudShell changes

--- billops/src/main/java/com/xammer/cloud/security/ClientUserDetails.java
+++ billops/src/main/java/com/xammer/cloud/security/ClientUserDetails.java
@@ -1,5 +1,8 @@
 package com.xammer.cloud.security;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.security.core.GrantedAuthority;
